490e2b8d143c85bd002c2e04e0b93e7f51692036
Alexander Serrano <serrano.alexander94@gmail.com>
2016-02-18 15:45:08 -0500

    Update AS_MH_ECE408_PROJ1.m

--- AS_MH_ECE408_PROJ1.m
+++ AS_MH_ECE408_PROJ1.m
@@ -13,6 +13,6 @@ Prof. Keene
 M = 4;                 % Modulation Order
 k = log2(M);           % # of bits per symbol
 numSC = 52;           % Number of OFDM subcarriers  (standard -> 52)
-cpLen = 32;            % OFDM cyclic prefix length
+cpLen = 16;            % OFDM cyclic prefix length
 maxBitErrors = 100;    % Maximum number of bit errors
 maxNumBits = 1e7;      % Maximum number of bits transmitted
